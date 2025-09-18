3661c97052ca8a3b502b6db50a6d6b1311a6d0a5
darrenzwc <darren.chan709@gmail.com>
2025-09-17 21:33:07 -0500

    implemented plotting and data stats

--- main.py
+++ main.py
@@ -4,25 +4,31 @@ import numpy as np
 from plot_gen import *
 
 def main():
-    parser = argparse.ArgumentParser(description="Input a CHGCAR_SUM file with -f argument and output file name with -o argument.")
-    parser.add_argument('-f', dest = 'filename', required= True, help = 'Input file path')
-    parser.add_argument('-o', dest ='dataname', required = True, help = 'Input output file name')
+    parser = argparse.ArgumentParser(description="Input a CHGCAR_SUM file with -f argument and output file name with -o argument unless you already have a data file (.npy)")
+    parser.add_argument('-f', dest = 'filename', required= True, help = 'file path')
+    parser.add_argument('-o', dest = 'dataname', required = False, help = 'output file name')
     args = parser.parse_args()
-    if len(sys.argv) != 5:
+    if len(sys.argv) == 5 or len(sys.argv) == 3:
+        if sys.argv[1] != '-f':
+            print("Incorrect usage. Please use the -f argument followed by the filename.")
+            sys.exit(1)
+        if len(sys.argv) == 5:
+            if(sys.argv[3] != '-o'):
+                print("Incorrect usage. Please use the -o argument followed by the output file name.")
+                sys.exit(1)
+        if not os.path.isfile(args.filename):
+            print(f"Error: File '{args.filename}' does not exist.")
+            sys.exit(1)
+        if len(sys.argv) == 5:
+            data = parse(args.filename)
+            np.save(args.dataname, data)
+            print("data file", args.dataname, "saved.")
+        else:
+            print("Loading data file...")
+            plot(args.filename)
+    else:
         print("Please input the correct number of arguments.")
-    if(sys.argv[1] != '-f'):
-            print("Incorrect usage. Please use the -f argument followed by the filename.")
-            sys.exit(1)
-    if(sys.argv[3] != '-o'):
-            print("Incorrect usage. Please use the -f argument followed by the filename.")
-            sys.exit(1)
-
-    if not os.path.isfile(args.filename):
-        print(f"Error: File '{args.filename}' does not exist.")
         sys.exit(1)
-    data = parse(args.filename)
-    np.save(args.dataname, data)
-    print("data file", args.dataname, "saved.")
 
 if __name__ == "__main__":
     main()
--- plot_gen.py
+++ plot_gen.py
@@ -10,7 +10,7 @@ def skip_lines(f, num_lines):
 
 def parse(f):
     file_path = f
-    data = np.array([])
+    raw_data = np.array([])
     num_data = 0
     write_data = False
     t_start = time.time()
@@ -24,13 +24,16 @@ def parse(f):
                     write_data = True
                 if write_data:
                     try:
-                        data = np.fromfile(file = f, dtype = np.float64, sep = ' ')
+                        raw_data = np.fromfile(file = f, dtype = np.float64, sep = ' ')
                     except ValueError:
                         print(f"Error in charge density data found on line {index + 1}, skipping line.")
     except Exception as e:
         print(f"Error reading file: {e}")
     t_elapsed = time.time() - t_start
-    print(f"Parsing Process Time: {t_elapsed:.3f} seconds")
+    neg_den = (raw_data < 0).sum()
+    data = raw_data[raw_data > 0]
+    print(f"Parser Processing Time: {t_elapsed:.3f} \nNegative densities removed from data\n{(neg_den / data.size):.2%} of the charge densities are negative ({neg_den})\n")
+    print(f"Stats\nFiltered data: {data.size} densities\nMin Density: {np.min(data)}\nMax Density: {np.max(data)}\nMedian: {np.median(data)}\n")
     plot(data)
     return data 
 
@@ -39,8 +42,11 @@ def plot(data):
     if isinstance(data, (str, bytes)) and data.endswith(".npy"):
         d = np.load(data)
     sns.histplot(data = d, bins = 1000, log_scale = True)
+    plt.xlabel("Electron Density (1/Å³)")
+    plt.ylabel("Frequency")
+    plt.title("Log-scaled Electron Density Distribution")
     plt.show()
-    plt.savefig("plot.png")
+    
 
     
         
